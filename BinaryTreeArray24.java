d679005dc731f956946d6916b4f377878d78638e
revaliya80 <revaliya80@gmail.com>
2026-05-31 17:50:42 +0700

    Praktikum 2

---
+++ BinaryTreeArray24.java
@@ -0,0 +1,23 @@
+public class BinaryTreeArray24 {
+    Mahasiswa24[] dataMahasiswa;
+    int idxLast;
+
+    public BinaryTreeArray24() {
+        this.dataMahasiswa = new Mahasiswa24[10];
+    }
+
+    void populateData(Mahasiswa24[] dataMhs, int idxLast) {
+        this.dataMahasiswa = dataMhs;
+        this.idxLast = idxLast;
+    }
+
+    void traverseInOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            if (dataMahasiswa[idxStart] != null) {
+                traverseInOrder(2 * idxStart + 1);
+                dataMahasiswa[idxStart].tampilInformasi();
+                traverseInOrder(2 * idxStart + 2);
+            }
+        }
+    }
+}
